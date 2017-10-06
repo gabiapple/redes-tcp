2ec7444212a67691167b940758e184007a96cfc5
Gabriela Ramalho <gabrielaramalho96@gmail.com>
2017-10-06 09:54:18 -0300

    Atualizando cliente-servidor para receber arquivo particionado

--- cliente.py
+++ cliente.py
@@ -2,16 +2,22 @@ import socket                   # Import socket module
 
 s = socket.socket()             # Create a socket object
 host = socket.gethostname()     # Get local machine name
-port = 8000                    # Reserve a port for your service.
+port = 8001                    # Reserve a port for your service.
 
 s.connect((host, port))
-s.send("Hello server!")
+#s.send("Hello server!")
+
+s.send('TMQ?')
+print 'recebendo TMQ'
+TMQ = s.recv(20)
+print TMQ
+
 
 with open('received_file', 'wb') as f:
     print 'file opened'
     while True:
         print('receiving data...')
-        data = s.recv(1024)
+        data = s.recv(int(TMQ))
         print('data=%s', (data))
         if not data:
             break
@@ -19,6 +25,7 @@ with open('received_file', 'wb') as f:
         f.write(data)
 
 f.close()
+
 print('Successfully get the file')
 s.close()
 print('connection closed')
--- servidor.py
+++ servidor.py
@@ -1,26 +1,32 @@
 import socket                   # Import socket module
 
-port = 8000                    # Reserve a port for your service.
+port = 8001                    # Reserve a port for your service.
 s = socket.socket()             # Create a socket object
 host = socket.gethostname()     # Get local machine name
 s.bind((host, port))            # Bind to the port
 s.listen(5)                     # Now wait for client connection.
+TMQ = '5'
+
 
 print 'Server listening....'
 
 while True:
     conn, addr = s.accept()     # Establish connection with client.
-    print 'Got connection from', addr
-    data = conn.recv(1024)
-    print('Server received', repr(data))
 
-    filename= raw_input()
+    print 'Got connection from', addr
+    message = conn.recv(20)
+    conn.send(TMQ)
+    
+ #   data = conn.recv(1024)
+ #   print('Server received', repr(data))
+
+    filename = 'arquivo.txt'
     f = open(filename,'rb')
-    l = f.read(1024)
+    l = f.read(int(TMQ))
     while (l):
        conn.send(l)
        print('Sent ',repr(l))
-       l = f.read(1024)
+       l = f.read(int(TMQ))
     f.close()
 
     print('Done sending')
