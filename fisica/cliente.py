40b8ec8eb0dffa994a0e00534d086869611ad203
Gabriela Ramalho <gabrielaramalho96@gmail.com>
2017-10-08 17:33:08 -0300

    Enviando do cliente para o servidor a msg como frame(binario) e imprimindo em string nos servidor

--- fisica/cliente.py
+++ fisica/cliente.py
@@ -1,31 +1,65 @@
 import socket                   # Import socket module
+import os
+import binascii
 from python_arptable import *
+
+def stringToBin(msg):
+    print msg
+    data_binary = bin(int(binascii.hexlify(msg),16)).split('b')
+    print data_binary
+    return data_binary
+
+def criaFrame(msg):
+    preambulo = '10101010101010101010101010101010101010101010101010101010'
+    start_frame = '10101011'
+    mac_orig = ''.join(get_arp_table()[0]['HW address'].split(':'))
+    mac_dest = ''.join(get_arp_table()[0]['HW address'].split(':'))
+    tipo = '0000000011111111'
+    frame = ""
+    frame += preambulo + '|' + start_frame + '|' + mac_orig + '|' + mac_dest + '|' + tipo + '|' + msg[1]
+    return frame
+
+def recebeMensagem():
+    filename = 'mensagem.txt'
+    f = open(filename,'rb')
+    msg = f.read()
+    f.close()
+    return msg
+
+
+msg = recebeMensagem()
+
+# converte para binario
+msg_bin = stringToBin(msg)
+
+# cria Frame Ethernet
+frame = criaFrame(msg_bin)
+
+with open('frameEnvio.txt', 'wb') as f:
+    print 'file opened'
+    f.write(frame)
 
 s = socket.socket()             # Create a socket object
 host = socket.gethostname()     # Get local machine name
-port = 8001                    # Reserve a port for your service.
+port = 8795                   # Reserve a port for your service.
 
 s.connect((host, port))
-#s.send("Hello server!")
 
+# Pergunta TMQ
 s.send('TMQ?')
 print 'recebendo TMQ'
-TMQ = s.recv(20)
+TMQ = s.recv(10)
 print TMQ
 
+# Envia frame para servidor
+filename = 'frameEnvio.txt'
+f = open(filename,'rb')
+l = f.read(int(TMQ))
+while (l):
+    s.send(l)
+    #print('Sent ',repr(l))
+    l = f.read(int(TMQ))
 
-with open('received_file', 'wb') as f:
-    print 'file opened'
-    while True:
-        print('receiving data...')
-        data = s.recv(int(TMQ))
-        print('data=%s', (data))
-        if not data or data == 'EOF':
-            break
-        # write data to a file
-        f.write(data)
-
-f.close()
-print('Successfully get the file')
+print('Arquivo enviado')
 s.close()
-print('connection closed')
+print('Conexao fechada')
--- fisica/servidor.py
+++ fisica/servidor.py
@@ -1,7 +1,15 @@
 import socket                   # Import socket module
+import time
+import binascii
 from python_arptable import *
 
-port = 8001                    # Reserve a port for your service.
+def binToString(data_binary):
+    n = int(data_binary,2)
+    data_hex = binascii.unhexlify('%x' %n)
+    return data_hex
+
+
+port = 8795                   # Reserve a port for your service.
 s = socket.socket()             # Create a socket object
 host = socket.gethostname()     # Get local machine name
 s.bind((host, port))            # Bind to the port
@@ -13,25 +21,32 @@ print 'Server listening....'
 
 while True:
     conn, addr = s.accept()     # Establish connection with client.
-
-    print 'Got connection from', addr
-    message = conn.recv(20)
+  
+    # simulando envio de Quadro Ethernet (como se fosse da camada superior)
+    print 'Estabeleceu conexao com', addr
+    message = conn.recv(10)
     conn.send(TMQ)
     
- #   data = conn.recv(1024)
- #   print('Server received', repr(data))
+    frame = ""
+    while True:
+        print('recebendo dados...')
+        part = conn.recv(int(TMQ))
+        frame += part
+       # print('part=%s', (part))
+        if not part or part == '':
+            break
+        # write data to a file
+        #f.write(data)
 
-    filename = 'arquivo.txt'
-    f = open(filename,'rb')
-    l = f.read(int(TMQ))
-    while (l):
-       conn.send(l)
-       print('Sent ',repr(l))
-       l = f.read(int(TMQ))
-    
-    f.close()
-    conn.send('EOF')
-    
-    print('Done sending')
-    conn.send('Thank you for connecting')
+#f.close()
+    print('Recebido frame com sucesso')
+
+    # separar frame (PREAMBULO, START_DELIMITER, MAC_ORIG, MAC_DEST)
+    data = frame.split('|')
+    msg_bin = data[5]
+    msg = binToString(msg_bin)
+    print msg
+
     conn.close()
+
+
