2b683320d7166edb330031c981d623cac700152e
curysamuel <32130850+curysamuel@users.noreply.github.com>
2017-10-12 14:19:29 -0300

    Add files via upload

--- fisica/cliente.py
+++ fisica/cliente.py
@@ -1,6 +1,7 @@
 import socket                   # Import socket module
 import os
 import binascii
+import datetime
 from python_arptable import *
 
 def stringToBin(msg):
@@ -14,7 +15,7 @@ def criaFrame(msg):
     mac_dest = stringToBin(''.join(get_arp_table()[0]['HW address'].split(':')))
     tipo = '0000000011111111'
     frame = ""
-    frame += preambulo + '|' + start_frame + '|' + mac_orig[1] + '|' + mac_dest[1] + '|' + tipo + '|' + msg[1]
+    frame += preambulo + '\n' + start_frame + '\n' + mac_orig[1] + '\n' + mac_dest[1] + '\n' + tipo + '\n' + msg[1]
     return frame
 
 def recebeMensagem():
@@ -24,40 +25,44 @@ def recebeMensagem():
     f.close()
     return msg
 
+with open('log_c.txt', 'wb') as g:
+    msg = recebeMensagem()
+    g.write('Arquivo aberto [' + str(datetime.datetime.now()) + ']' + '\n')
+    # converte para binario
+    msg_bin = stringToBin(msg)
 
-msg = recebeMensagem()
+    # cria Frame Ethernet
+    frame = criaFrame(msg_bin)
+    with open('frameEnvio.txt', 'wb') as f:
+        print 'file opened'
+        f.write(frame)
 
-# converte para binario
-msg_bin = stringToBin(msg)
+    s = socket.socket()             # Create a socket object
+    host = socket.gethostname()     # Get local machine name
+    port = 8520                     # Reserve a port for your service.
 
-# cria Frame Ethernet
-frame = criaFrame(msg_bin)
+    s.connect((host, port))
+    g.write('Estabelece conexao [' + str(datetime.datetime.now()) + ']' + '\n')
 
-with open('frameEnvio.txt', 'wb') as f:
-    print 'file opened'
-    f.write(frame)
+    # Pergunta TMQ
+    
+    s.send('TMQ?')
+    g.write('Pergunta TMQ [' + str(datetime.datetime.now()) + ']' + '\n')
+    print 'recebendo TMQ'    
+    TMQ = s.recv(10)
+    g.write('Recebe TMQ [' + str(datetime.datetime.now()) + ']' + '\n')
 
-s = socket.socket()             # Create a socket object
-host = socket.gethostname()     # Get local machine name
-port = 8795                   # Reserve a port for your service.
-
-s.connect((host, port))
-
-# Pergunta TMQ
-s.send('TMQ?')
-print 'recebendo TMQ'
-TMQ = s.recv(10)
-print TMQ
-
-# Envia frame para servidor
-filename = 'frameEnvio.txt'
-f = open(filename,'rb')
-l = f.read(int(TMQ))
-while (l):
-    s.send(l)
-    #print('Sent ',repr(l))
+    # Envia frame para servidor
+    filename = 'frameEnvio.txt'
+    f = open(filename,'rb')
     l = f.read(int(TMQ))
-
-print('Arquivo enviado')
-s.close()
-print('Conexao fechada')
+    while (l):       
+        s.send(l)
+        g.write('Envia quadro para servidor [' + str(datetime.datetime.now()) + ']' + '\n')
+        #print('Sent ',repr(l))
+        l = f.read(int(TMQ))
+    g.write('Arquivo enviado [' + str(datetime.datetime.now()) + ']' + '\n')
+    print('Arquivo enviado')
+    s.close()
+    g.write('Conexao fechada [' + str(datetime.datetime.now()) + ']' + '\n')
+    print('Conexao fechada')
