07f3c33939d741858f0f6420618816d46787f3d9
Gabriela Ramalho <gabrielaramalho96@gmail.com>
2017-11-02 14:50:27 -0200

    Corrigindo mensagens de log, adicionando cabecalho para arquivo nao encontrado

--- aplicacao/cliente.rb
+++ aplicacao/cliente.rb
@@ -9,22 +9,22 @@ end
 server = TCPServer.new 5678
 puts "esperando cliente abrir o browser"
 loop{
-    session, addr = server.accept # cliente abriu o browser
+    session = server.accept # cliente abriu o browser
     log = File.open("log_cApp.txt", 'a')
-    log.write("Cliente abriu o browser:  #{addr} [ #{Time.now} ]\n")
+    log.write("Cliente abriu o browser [ #{Time.now} ]\n")
     puts "cliente abriu o browser\n"
     request = session.gets
-    log.write("Recebeu requisicao do cliente:  #{addr} [ #{Time.now} ]\n")
+    log.write("Recebeu requisicao do cliente [ #{Time.now} ]\n")
     puts request + '\n'
     camada_inferior = TCPSocket.new('localhost', 10000)
-    log.write("Estabeleceu conexao com camada inferior [ #{addr} [ #{Time.now} ]\n")
+    log.write("Estabeleceu conexao com camada inferior [ #{Time.now} ]\n")
     camada_inferior.print "#{request}"
-    log.write("Enviou solicitacao a camada inferior: [ #{addr} [ #{Time.now} ]\n")
+    log.write("Enviou solicitacao a camada inferior [ #{Time.now} ]\n")
    
     # resposta
     msg = ""
     msg = camada_inferior.recv(1024)
-    log.write("Recebeu resposta da camada inferior: [ #{addr} [ #{Time.now} ]\n")
+    log.write("Recebeu resposta da camada inferior [ #{Time.now} ]\n")
     
     print msg
     camada_inferior.close
--- aplicacao/servidor.rb
+++ aplicacao/servidor.rb
@@ -11,24 +11,43 @@ end
 loop{
 	log = File.open("log_sApp.txt", 'a')
 	session, addr = server.accept
-    log.write("Estabeleceu conexao com a camada inferior:  #{addr} [ #{Time.now} ]\n")
+    log.write("Estabeleceu conexao com a camada inferior [ #{Time.now} ]\n")
 	puts "server.accept"
 	request = session.recv(1024)
-	log.write("Recebeu requisicao da camada inferior:  #{addr} [ #{Time.now} ]\n")
+	log.write("Recebeu requisicao da camada inferior [ #{Time.now} ]\n")
 	
 	puts "request: " + request
-
+	part = request.split(' ')
+	puts 'part[2]: ' + part[2] + '\n'
 	print "Gerando PDU da aplicacao:\n"
-	puts "HTTP/1.1 200 OK\r\n" +
-               "Content-Type: text/html\r\n" +
-               "Content-Length: #{content.bytesize}\r\n" +
-               "Connection: close\r\n\r\n" 
-	session.print  "HTTP/1.1 200 OK\r\n" +
+	
+	case part[1]
+		when '/','/index.html'
+
+			puts "HTTP/1.1 200 OK\r\n" +
+		               "Content-Type: text/html\r\n" +
+		               "Content-Length: #{content.bytesize}\r\n" +
+		               "Connection: close\r\n\r\n" 
+			session.print  "HTTP/1.1 200 OK\r\n" +
                "Content-Type: text/html\r\n" +
                "Content-Length: #{content.bytesize}\r\n" +
                "Connection: close\r\n\r\n" +
                "#{content}"
-	log.write("Enviou resposta para camada inferior:  #{addr} [ #{Time.now} ]\n")
+		else
+			message = "File not found\n"
+			puts "HTTP/1.1 404 Not Found\r\n" +
+                 "Content-Type: text/plain\r\n" +
+                 "Content-Length: #{message.size}\r\n" +
+                 "Connection: close\r\n"
+			# respond with a 404 error code to indicate the file does not exist
+    		session.print "HTTP/1.1 404 Not Found\r\n" +
+                 "Content-Type: text/plain\r\n" +
+                 "Content-Length: #{message.size}\r\n" +
+                 "Connection: close\r\n\r\n" + 
+                 "#{message}"
+        end
+
+	log.write("Enviou resposta para camada inferior [ #{Time.now} ]\n")
 	session.close
 	log.close()
 }
